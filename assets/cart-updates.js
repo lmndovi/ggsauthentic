10314da46dc7e15814c094d1f170743abee28673
lmndovi <l.ndovi92@gmail.com>
2025-11-13 11:26:49 +0000

    Found additional fetch firing from cart-updates

--- assets/cart-updates.js
+++ assets/cart-updates.js
@@ -15,27 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
   updateCartCount();
 });
 
-// Listen for form submissions to update the cart count
-document.addEventListener("submit", function (event) {
-  if (event.target.matches('form[action="/cart/add"]')) {
-    event.preventDefault(); // Prevent default form submission
-
-    const formData = new FormData(event.target);
-    const variantId = formData.get("id");
-    const quantity = formData.get("quantity") || 1;
-
-    fetch("/cart/add.js", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({ id: variantId, quantity: quantity }),
-    })
-      .then((response) => response.json())
-      .then(() => {
-        updateCartCount(); // Update cart count after adding item
-      })
-      .catch((error) => console.error("Error adding item to cart:", error));
-  }
+// Listen for cart add events dispatched by theme.liquid
+document.addEventListener("ggs:cart-item-added", function () {
+  updateCartCount();
 });
